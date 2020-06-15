e0ffe8df572201ce16509ef5679368ffbec85b0c
Emeka Ugwuanyi <emylincon@gmail.com>
2020-06-15 21:59:59 +0100

    updated

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+apt update && apt upgrade -y
+apt install python3-pip -y
+apt install nano -y
+apt install nginx -y
+pip3 install flask
+pip3 install flask-restful
+pip3 install gunicorn
+
+rm /etc/nginx/sites-enabled/default
+mv flask_settings /etc/nginx/sites-available/
+ln -s /etc/nginx/sites-available/flask_settings /etc/nginx/sites-enabled/flask_settings
+/etc/init.d/nginx restart
