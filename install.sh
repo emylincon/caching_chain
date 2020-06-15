ce4dccfdd8265fcb4e6feeeb5e698ef9f53ccdea
Emeka Ugwuanyi <emylincon@gmail.com>
2020-06-15 19:53:52 +0100

    updated install

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-apt update && apt upgrade
-apt install python3-pip
-apt install apache2
-apt install Libapache2-mod-wsgi-py3
+apt update && apt upgrade -y
+apt install python3-pip -y
+apt install apache2 -y
+apt install Libapache2-mod-wsgi-py3 -y
+pip3 install flask
+pip install flask-restful
