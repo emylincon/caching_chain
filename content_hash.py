3b769348fd60453dea26b48a8a0e69fa8f0d035e
Emeka Ugwuanyi <emylincon@gmail.com>
2020-06-17 09:34:02 +0100

    improved content hash

--- content_hash.py
+++ content_hash.py
@@ -1,10 +1,18 @@
 
 class Content:
     def __init__(self):
-        self.store = {}
+        self.store = {}        # cache_id : content_id
+        self.reverse_store = {}     # content_id: url
 
-    def add_node(self, data):
-        self.store.update(data)
+    def add_node(self, s_data, r_data):
+        self.store.update(s_data)
+        self.reverse_store.update(r_data)
+
+    def read_reserve(self, key):
+        if key in self.reverse_store:
+            return {'url': self.reverse_store[key]}
+        else:
+            return {'url': None}
 
     def read_node(self, key):
         if key in self.store:
